255d505519e4a83f7a3c077ed938c16ff5c0e81d
Changs <leechangs10@naver.com>
2020-07-21 21:54:42 +0900

    first commit

---
+++ js/my.js
@@ -0,0 +1,20 @@
+
+        // 자바 스크립트 코드.. (데이터 타입, 연산자, 제어문, 함수)
+        // 명명규칙 (a-z, A-Z, 0-9, _한글)
+
+        a=10
+        f=3.14
+        s= 'abcd' // 문자열 타입
+        my=[10,20,30,40]  // 파이썬에서는 리스트, 자바스크립트에서는 배열(array)
+        j ={ "a":10,  "b":20}  // 파이썬에서는 딕셔너리, 자바스크립트에서는 제이슨 (JSON)
+        function fn(){ // 자바스크립트 함수 선언
+
+        }
+
+        console.log ("a=", a)
+        console.log(typeof(a))
+        console.log("f=",f, typeof(f))
+        console.log ("s=",s,typeof(s))
+        console.log("my=",my, typeof(my))
+        console.log("j=",j, typeof(j))
+        console.log("fn=",fn, typeof(fn))
